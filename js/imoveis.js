49ad5810ef5b7ac1112ff43c08e2e0ba8b1f60d2
B1naryo <sandro520099@hotmail.com>
2024-01-31 09:58:33 -0300

    Update imoveis.js

--- js/imoveis.js
+++ js/imoveis.js
@@ -199,6 +199,15 @@ function init() {
 }
 
 // Criação de pergunta
+// Função para embaralhar um array usando o algoritmo de Fisher-Yates
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// Criação de pergunta
 function createQuestion(index) {
   clearPreviousQuestion();
   const question = questions[index];
@@ -207,6 +216,9 @@ function createQuestion(index) {
 
   questionText.textContent = question.question;
   questionNumber.textContent = index + 1;
+
+  // Embaralha as respostas antes de exibi-las
+  shuffle(question.answers);
 
   question.answers.forEach((answer, i) => {
     const answerTemplate = document.querySelector('.answer-template').cloneNode(true);
@@ -275,36 +287,12 @@ function checkAnswer(btn) {
   }
 }
 
-// Exibe a tela final
-function showSuccessMessage() {
-  toggleQuizzVisibility();
+// Restante do seu código...
 
-  const score = ((points / questions.length) * 100).toFixed(2);
-  const displayScore = document.querySelector('#display-score span');
-  displayScore.textContent = score.toString();
-
-  const correctAnswers = document.querySelector('#correct-answers');
-  correctAnswers.textContent = points;
-
-  const totalQuestions = document.querySelector('#questions-qty');
-  totalQuestions.textContent = questions.length;
+// Função para inicializar o quizz
+function init() {
+  createQuestion(actualQuestion);
 }
-
-// Mostra ou esconde o score
-function toggleQuizzVisibility() {
-  quizzContainer.classList.toggle('hide');
-  scoreContainer.classList.toggle('hide');
-}
-
-// Reiniciar quizz
-const restartBtn = document.querySelector('#restart');
-restartBtn.addEventListener('click', function () {
-  actualQuestion = 0;
-  points = 0;
-  toggleQuizzVisibility();
-  init();
-});
 
 // Inicialização do quizz
 init();
-
